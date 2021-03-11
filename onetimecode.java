d26164f522b586eb446a387f4dc1af58328f1f2a
robbie <robbielcampbell@hotmail.com>
2021-03-11 12:30:40 +0000

    :shipit: fixed onetimecode :shipit:

--- onetimecode.java
+++ onetimecode.java
@@ -1,45 +1,32 @@
 public class onetimecode{
 
-    public static String encrypt(String plaincode, String pad){
-        
-        StringBuilder cypherText = new StringBuilder();
-        char[] pcarr = plaincode.toCharArray();
-        char[] padarr = pad.toCharArray();
-        for (int i = 0; i < plaincode.toCharArray().length; i++)
-        {
-            if (i < 5)
-            {
-                cypherText.append(String.valueOf(padarr[i]));
-            }
-            else
-            {
-                int encryptVal = Integer.parseInt(String.valueOf(pcarr[i])) - Integer.parseInt(String.valueOf(padarr[i])) % 10;
-                int finalVal =  encryptVal < 0 ? 10 - (encryptVal * -1) : encryptVal;
-                cypherText.append(finalVal);
-            }
-        }
-        System.out.println(cypherText.toString() == "6371956289367449331922");
-        return String.valueOf(cypherText);
-    }
+    public static String encrypt(String plncode, String pad) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(pad.substring(0,5));
+		for (int i=0 ; i<plncode.length() ; i++) {
+			 sb.append(checkEncrypt(Integer.parseInt(plncode.substring(i, i+1)), Integer.parseInt(pad.substring(i+5, i+6))));
+		}
+		return sb.toString();
+	}
 
-    public static StringBuilder decrypt(String cypcode, String pad){
-        StringBuilder plaintext = new StringBuilder();
-        char[] pcarr = cypcode.toCharArray();
-        char[] padarr = pad.toCharArray();
-        for (int i = 0; i < cypcode.toCharArray().length; i++)
-        {
-            if (i < 5)
-            {
-                plaintext.append(String.valueOf(padarr[i]));
-            }
-            else
-            {
-                int encryptVal = Integer.parseInt(String.valueOf(pcarr[i])) + Integer.parseInt(String.valueOf(padarr[i])) % 10;
-                plaintext.append(encryptVal);
-            }
-        }
-        return plaintext;
-    }
+	private static Object checkEncrypt(int plncode, int pad) {
+		int encrpt = plncode-pad;
+		return encrpt >= 0 ? encrpt : 10 + encrpt;
+	}
+
+	public static String decrypt (String cypcode, String pad) {
+		StringBuilder sb = new StringBuilder();
+		if (!cypcode.substring(0,5).equals(pad.substring(0,5))) return "Error: Key IDs don't match.";
+		for (int i=5 ; i<cypcode.length() ; i++) {
+			 sb.append(checkDecrypt(Integer.parseInt(cypcode.substring(i, i+1)), Integer.parseInt(pad.substring(i, i+1))));
+		}
+		return sb.toString();
+	}
+
+	private static Object checkDecrypt(int cypcode, int pad) {
+		int decrypt = cypcode + pad;
+		return decrypt < 10 ? decrypt :  decrypt-10;
+	}
 
     public static void main(String[] args)
     {
