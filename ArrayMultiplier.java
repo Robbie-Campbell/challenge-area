312b5669a7244398baed8b2638a561f2fa4e17e5
robbie <robbielcampbell@hotmail.com>
2021-03-15 10:35:23 +0000

    :shipit: another challenge :shipit:

---
+++ ArrayMultiplier.java
@@ -0,0 +1,27 @@
+// https://edabit.com/challenge/5mANMR3X6gEfWephD
+import java.util.Arrays;
+
+public class ArrayMultiplier
+{
+
+    public static String multiply(Object[] values)
+    {
+        int sizeOfArr = values.length;
+        Object[][] multiplied = new Object[sizeOfArr][sizeOfArr];
+        for (int i = 0; i < sizeOfArr; i++)
+        {
+            for (int x = 0; x < sizeOfArr; x++)
+            {
+                multiplied[i][x] = values[i];
+            }
+        }
+        return Arrays.deepToString(multiplied);
+
+    }
+
+    public static void main(String[] args)
+    {
+        Object[] object = {1, 3, "hello"};
+        System.out.println(multiply(object));
+    }
+}
---
+++ SecondLargest.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SecondLargest 
+{
+
+    public static int secondLargest(int[] values)
+    {
+        Arrays.sort(values);
+        return values[1];
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] tester = {3,6,7,4,9};
+        System.out.println(secondLargest(tester));
+    }
+}
