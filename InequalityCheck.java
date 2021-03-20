1d974cec6efa8118755dbeb21df01a79b4e291e5
robbie <robbielcampbell@hotmail.com>
2021-03-20 12:32:38 +0000

    :shipit: another one done :shipit:

---
+++ InequalityCheck.java
@@ -0,0 +1,26 @@
+public class InequalityCheck
+{
+    public static boolean correctSigns(String str)
+    {
+        String[] newString = str.split(" ");
+        for (int i = 0; i < newString.length; i++)
+        {
+            String trimVal = newString[i].trim();
+            if(trimVal.equals("<") && Integer.parseInt(newString[i+1]) <= Integer.parseInt(newString[i - 1]))
+            {
+                return false;
+            }
+            else if(trimVal.equals(">") && Integer.parseInt(newString[i+1]) >= Integer.parseInt(newString[i - 1]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(correctSigns("3 < 2 < 11"));
+        System.out.println(correctSigns("3 < 15 > 11"));
+    }
+}
