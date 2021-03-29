abd767de35edaf878ccc3cc491ec0a92c1a9913c
robbie <robbielcampbell@hotmail.com>
2021-03-29 10:27:29 +0100

    :art: now with concatination :art:

--- java_work/Concatinator.java
+++ java_work/Concatinator.java
@@ -8,8 +8,8 @@ public class Concatinator
     public static String inatorInator(String inv)
     {
         if (!String.valueOf(inv.charAt(inv.length() - 1)).matches("[^aeiouAEIOU]+"))
-            return String.format("%s-inator %d000", inv, inv.length());
-        return String.format("%sinator %d000", inv, inv.length());
+            return inv + "-inator " + inv.length() + "000";
+        return inv + "inator " + inv.length() + "000";
     }
 
     public static void main(String[] args)
