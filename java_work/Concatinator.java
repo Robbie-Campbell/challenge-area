3d66ec6e85b97c466ffe436571cf984f5ab8c830
robbie <robbielcampbell@hotmail.com>
2021-03-29 10:23:43 +0100

    :bento: didn't read it properly :bento:

--- java_work/Concatinator.java
+++ java_work/Concatinator.java
@@ -7,11 +7,14 @@ public class Concatinator
 {
     public static String inatorInator(String inv)
     {
+        if (!String.valueOf(inv.charAt(inv.length() - 1)).matches("[^aeiouAEIOU]+"))
+            return String.format("%s-inator %d000", inv, inv.length());
         return String.format("%sinator %d000", inv, inv.length());
     }
 
     public static void main(String[] args)
     {
         System.out.println(inatorInator("Shrink"));
+        System.out.println(inatorInator("EvilClone"));
     }
 }
