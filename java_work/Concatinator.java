5491d60bfb0bde16690ce5f0f3d489063bc6f431
robbie <robbielcampbell@hotmail.com>
2021-03-29 10:15:06 +0100

    Very easy

---
+++ java_work/Concatinator.java
@@ -0,0 +1,17 @@
+package java_work;
+
+//https://edabit.com/challenge/ynzo3DeHCmAXLtpwe
+// Yes, this is especially easy
+
+public class Concatinator 
+{
+    public static String inatorInator(String inv)
+    {
+        return String.format("%sinator %d000", inv, inv.length());
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(inatorInator("Shrink"));
+    }
+}
