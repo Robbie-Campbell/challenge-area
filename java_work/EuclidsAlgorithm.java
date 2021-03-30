bd033fb6820d71e22474b2546d89663402b29ac1
robbie <robbielcampbell@hotmail.com>
2021-03-30 11:07:36 +0100

    :pencil: part of a lecture :pencil:

---
+++ java_work/EuclidsAlgorithm.java
@@ -0,0 +1,40 @@
+package java_work;
+
+import java.util.Arrays;
+
+public class EuclidsAlgorithm {
+    public int getCommonFactor(int a, int b) 
+    {
+        int i = a;
+        while (i > 0) 
+        {
+            if (a % i == 0 && b % i == 0)
+                return i;
+            i--;
+        }
+        return 1;
+    }
+
+    public int getCommonFactor(int[] a) {
+        Arrays.sort(a);
+        int i = a[a.length - 1];
+        while (i > 0) {
+            for (int j = 0; j < a.length; j++) {
+                if (j == a.length - 1 && a[j] % i == 0)
+                    return i;
+                else if (a[j] % i == 0)
+                    continue;
+                else
+                    break;
+            }
+            i--;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        EuclidsAlgorithm test = new EuclidsAlgorithm();
+        System.out.println(test.getCommonFactor(12, 12));
+        // System.out.println(test.getCommonFactor(new int[]{30,200,300,333,20}));
+    }
+}
