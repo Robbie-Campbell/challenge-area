7a2dcaddc913725c5708485744ed9468eb47ac79
robbie <robbielcampbell@hotmail.com>
2021-04-02 09:47:34 +0100

    :bento: another easy one :bento:

---
+++ java_work/Calculator.java
@@ -0,0 +1,16 @@
+package java_work;
+
+public class Calculator
+{
+    public static int calculator(int num1, char operator, int num2) 
+    {
+        if (num2 == 0 && operator == '/')
+            return 0;
+		return operator == '-' ? num1 - num2 : operator == '+' ? num1 + num2 : operator == '/' ? num1 / num2 : num1 * num2;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(calculator(2, '/', 2));
+    }
+}
