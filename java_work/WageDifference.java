88453bb08af9b794de65fbaaa4d84271ba41a06f
robbie <robbielcampbell@hotmail.com>
2021-04-03 11:54:23 +0100

    :shipit: easy one :shipit:

---
+++ java_work/WageDifference.java
@@ -0,0 +1,18 @@
+package java_work;
+
+// https://edabit.com/challenge/akHQKSkHT26TuA7Ka
+
+public class WageDifference 
+{
+    public static int programmers(int one, int two, int three) 
+    {
+		int biggest = (one < two && three < two) ? two : (two < one && three < one) ? one : three;
+		int smallest = (one < two && one < three) ? one : (two < one && two < three) ? two : three;
+        return biggest - smallest;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(programmers(187, 50, 1200));
+    }
+}
