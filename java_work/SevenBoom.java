083aeca52a06aae367ab80f5f76045546292f6ab
robbie <robbielcampbell@hotmail.com>
2021-04-04 11:40:34 +0100

    :shipit: done :shipit:

---
+++ java_work/SevenBoom.java
@@ -0,0 +1,22 @@
+package java_work;
+
+public class SevenBoom
+{
+    public static String sevenBoom(int[] values)
+    {
+        for (int value: values)
+        {
+            if (String.valueOf(value).contains("7"))
+                return "Boom!";
+            else
+                continue;
+        }
+        return "There was no 7 in the array";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(sevenBoom(new int[]{8, 6, 33, 100}));
+        System.out.println(sevenBoom(new int[]{2, 55, 60, 97, 86}));
+    }
+}
