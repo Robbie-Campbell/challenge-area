612f86c42fc49c0a8c7f124c44639c72927ce0bf
robbie <robbielcampbell@hotmail.com>
2021-04-13 10:33:02 +0100

    :shipit: done :shipit:

---
+++ java_work/GetWordCount.java
@@ -0,0 +1,13 @@
+package java_work;
+
+public class GetWordCount
+{
+    public static int countWords(String s) {
+		return s.split(" ").length;
+	}
+
+    public static void main(String[] args)
+    {
+        System.out.println(countWords("Hello there i am rab"));
+    }
+}
