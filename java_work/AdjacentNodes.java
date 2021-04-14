68dc595da0aab13e557e8afa6d050ca9ad646acf
robbie <robbielcampbell@hotmail.com>
2021-04-14 13:42:00 +0100

    :shipit: messed with my head :shipit:

---
+++ java_work/AdjacentNodes.java
@@ -0,0 +1,26 @@
+// https://edabit.com/challenge/8hk27ha9CtqCQsYP8
+/*
+    Trying to understand this one was a nightmare, but the solution
+    is very very easy.
+*/
+
+package java_work;
+
+public class AdjacentNodes
+{
+    public static boolean adjacent(int[][] graph, int node1, int node2) 
+    {
+        return graph[node1][node2] == 1 ? true : false;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(adjacent(new int[][]{
+                { 0, 1, 0, 0 },
+                { 1, 0, 1, 1 },
+                { 0, 1, 0, 1 },
+                { 0, 1, 1, 0 }
+            }, 1, 3
+        ));
+    }
+}
