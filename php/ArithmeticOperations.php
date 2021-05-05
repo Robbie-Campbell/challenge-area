67387ffcb433aa606ae75a23e30d084af7ae0855
robbie <robbielcampbell@hotmail.com>
2021-05-05 18:14:22 +0100

    :shipit: completed :shipit:

---
+++ php/ArithmeticOperations.php
@@ -0,0 +1,24 @@
+<?php
+    function arithmeticOperation($n) 
+    {   
+        $operation = explode(" ", $n);
+        switch ($operation[1])
+        {
+            case "+":
+                return (int)$operation[0] + (int)$operation[2];
+            case "-":
+                return (int)$operation[0] - (int)$operation[2];
+            case "/":
+                if ($operation[2] == 0)
+                    return -1;
+                return (int)$operation[0] / (int)$operation[2];
+            case "*":
+                return (int)$operation[0] * (int)$operation[2];
+        }
+    }
+
+    echo arithmeticOperation("12 + 12");
+    echo arithmeticOperation("12 - 12");
+    echo arithmeticOperation("12 * 12");
+    echo arithmeticOperation("12 / 0");
+?>
